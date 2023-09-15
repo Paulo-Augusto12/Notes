bac361490022a7e0ded5677a7899a9575bc785e2
Paulo Augusto <paulo-augusto12@outlook.com>
2023-09-15 14:55:52 -0300

    chore: using api folder for study backend development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import { Header } from "../components/header";
 import { Sidebar } from "../components/sidebar";
 import { NoteCard } from "../components/noteCard";
+import { HttpService } from "../services/http/httpService";
 
-export default function Home() {
+type x = {
+  notes: {
+    title: string;
+    description: string;
+  }[];
+};
+export default async function Home() {
+  const response = await HttpService<x>("/getnotes", {});
+
   return (
     <main className="flex flex-col min-h-screen">
       <Header />
       <div className="flex flex-1 flex-row">
         <Sidebar />
-        <div className="flex flex-wrap flex-col px-4 py-8">
-          <NoteCard />
+        <div className="flex flex-wrap flex-row justify-start items-start gap-4 px-4 py-8">
+          {response.notes.map(({description, title}) => (
+            <NoteCard />
+          ))}
         </div>
       </div>
     </main>
---
+++ src/services/http/httpService.ts
@@ -0,0 +1,12 @@
+export async function HttpService<T>(url: string, body: any) {
+  const route = `http://localhost:3000/api${url}`;
+  console.log(route, "[aqui]");
+  const response = await fetch(`http://localhost:3000/api${url}`);
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  const data: T = await response.json();
+
+  return data;
+}
