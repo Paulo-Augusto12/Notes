2f8b935064a3ecd8b3741b2a4e311596958f371e
Paulo Augusto <paulo-augusto12@outlook.com>
2023-09-19 14:51:16 -0300

    refactor: changed env variable being used on api wrapper

--- src/services/http/httpService.ts
+++ src/services/http/httpService.ts
@@ -1,5 +1,5 @@
-export async function HttpService<T>(url: string, config?: RequestInit) {
-  const route = `${process.env.API_BASE_URL}${url}`;
+export async function HttpService<T = any>(url: string, config?: RequestInit) {
+  const route = `${process.env.NEXT_PUBLIC_API_BASE_URL}${url}`;
   const response = await fetch(route, config);
 
   if (!response.ok) {
