ac878f49178c24341e75771a98182239b4caaa11
Paulo Augusto <paulo-augusto12@outlook.com>
2023-09-19 10:08:23 -0300

    chore: developed initial verison of add a note page

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -3,11 +3,15 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "../../components/ui/card";
 import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
+import { Textarea } from "../../components/ui/textarea";
+import { Form } from "./components/form";
+import Link from "next/link";
 
 export default function Add() {
   return (
@@ -19,17 +23,40 @@ export default function Add() {
             Descreva da forma desejada sobre a sua nova anotação
           </CardDescription>
         </CardHeader>
-        <CardContent>
-          <form>
-            <Label htmlFor="title">Título</Label>
-            <Input id="title" />
-          </form>
-        </CardContent>
+        <Form>
+          <CardContent className="flex flex-col space-y-8">
+            <div>
+              <Label htmlFor="title">Título</Label>
+              <Input id="title" name="title" />
+            </div>
+            <div>
+              <Label htmlFor="content">Conteúdo</Label>
+              <Textarea
+                id="content"
+                className="min-h-[250px] resize-none"
+                name="content"
+              />
+              <span className="text-muted-foreground">
+                O campo acima possui suporte a{" "}
+                <Link href={"https://www.markdownguide.org/"}>
+                  <code className="text-secondary-foreground cursor-pointer">
+                    Markdown
+                  </code>
+                </Link>
+              </span>
+            </div>
+
+            <CardFooter className="flex justify-end gap-2">
+              <Button type="submit">Adicionar</Button>
+              <Link href={"/"}>
+                <Button variant={"secondary"} type="reset">
+                  Cancelar
+                </Button>
+              </Link>
+            </CardFooter>
+          </CardContent>
+        </Form>
       </Card>
-      <div className="flex flex-row justify-end gap-2">
-        <Button>Adicionar</Button>
-        <Button variant={"secondary"}>Cancelar</Button>
-      </div>
     </div>
   );
 }
