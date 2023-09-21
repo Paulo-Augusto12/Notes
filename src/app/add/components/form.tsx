dd1da66cad6bec6cae35f570eaceb188f71ab50c
Paulo Augusto <paulo-augusto12@outlook.com>
2023-09-21 13:28:10 -0300

    feat: using toast on form

--- src/app/add/components/form.tsx
+++ src/app/add/components/form.tsx
@@ -2,19 +2,38 @@
 
 import { FormEvent, ReactNode } from "react";
 import { HttpService } from "../../../services/http/httpService";
+import { useToast } from "../../../components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 interface IFormProps {
   children: ReactNode;
 }
 export function Form({ children }: IFormProps) {
+  const { toast } = useToast();
+  const { back } = useRouter()
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    await HttpService("/addnote", {
-      method: "POST",
-      body: formData,
-      cache: 'no-cache'
-    });
+    try {
+      await HttpService("/addnote", {
+        method: "POST",
+        body: formData,
+        cache: "no-cache",
+      });
+
+      toast({
+        title: "Nota adicionada com sucesso",
+        description:
+          "você será redirecionado para a sua lista de novas para conferir sua nova anotação",
+      });
+      back()
+    } catch (err) {
+      toast({
+        variant: "destructive",
+        title: "Oh, algo deu errado",
+        description: "Por favor tente de novo mais tarde",
+      });
+    }
   }
   return (
     <form className="flex gap-8 flex-col" onSubmit={handleSubmit}>
